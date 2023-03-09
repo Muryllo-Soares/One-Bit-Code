1e18bbdcebc39945c1a9910cabea7802dc772fcb
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-09 14:50:57 -0300

    ex cadastro de recruta

---
+++ JS I - Lógica de programação/ex001/script.js
@@ -0,0 +1,14 @@
+
+const nome = prompt('Primeiro nome')
+const sobreNome = prompt('SobreNome')
+const estudo = prompt('Qual campo de estudo?')
+const nascimento = prompt('qual ano de nascimento?')
+
+
+alert(
+    "Recruta cadastro com sucesso!\n" +
+    "\n Nome completo: " + nome + " " + sobreNome +
+    "\n Crampo de estudo: " + estudo +
+    "\n Idade: " + (2023 - nascimento)
+)
+    
