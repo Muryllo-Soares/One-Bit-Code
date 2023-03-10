21b4f4d2c9d9b999ee2a6963cb12f5ef79d75371
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-10 15:12:17 -0300

    ex switch

--- JS I - Lógica de programação/ex004/script.js
+++ JS I - Lógica de programação/ex004/script.js
@@ -1,13 +1,15 @@
+const resultado = prompt ("Escolha uma das alternativas \nA \nB \nC")
 
-
-switch ("b"){
+switch (resultado){
     case "a":
         alert ("resposta 'a'")
+        break
     case "b":
         alert ("resposta 'b'")
         break
     case "c":
         alert ("resposta 'c'")
+        break
     default: 
     alert ("Finalizando...")
 
