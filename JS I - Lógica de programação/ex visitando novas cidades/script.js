6e05d9a4d6cb8d73e1d7532633bbc6bfeac63404
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-11 09:18:07 -0300

    ex Visitando Novas Cidades

---
+++ JS I - Lógica de programação/ex visitando novas cidades/script.js
@@ -0,0 +1,27 @@
+/* const turista = prompt("E ai turista, Qual o seu nome?");
+let cidades = "";
+let contagem = 0;
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)");
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual cidade?");
+  cidades += " - " + cidade + "\n";
+  contagem++;
+  continuar = prompt("Visitou mais alguma cidade (Sim/Não)");
+}
+
+alert(
+  "Nome do Turista: " +
+    turista +
+    "\n" +
+    "\n N° de cidades visitadas: " +
+    contagem +
+    "\n Cidades visitadas:\n " +
+    cidades
+); */
+let contagem = 10
+while (contagem >= 0) {
+  contagem++;
+  console.log(contagem);
+}
