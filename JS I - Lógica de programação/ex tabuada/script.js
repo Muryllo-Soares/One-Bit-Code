a991f11416ffb2d9cd926042fb84c08d431a8ad7
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-14 12:47:15 -0300

    ex de tabuada

---
+++ JS I - Lógica de programação/ex tabuada/script.js
@@ -0,0 +1,10 @@
+let n =  2;
+let res = ""
+
+for(let i= 1 ; i <= 20 ; i++){
+    res += + n + " X " + i + " = " + n*i +"\n"
+}
+alert(res)
+
+
+
--- JS I - Lógica de programação/ex006/for.js
+++ JS I - Lógica de programação/ex006/for.js
@@ -1,5 +1,4 @@
 let n = 2
-
-for (let i = 1; i <= 20 ; i++){
-    console.log(n + " x " + i + " = " + n*i)
+for (let i = 1 ; i <= 20 ; i++){
+    alert(n + " x " + i + " = " + n*i);
 }
