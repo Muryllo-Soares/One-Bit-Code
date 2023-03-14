c929fd8c471895e695c6b44f8f8e990e800fbffa
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-14 13:50:12 -0300

    Update script.js

--- JS I - Lógica de programação/ex tabuada/script.js
+++ JS I - Lógica de programação/ex tabuada/script.js
@@ -1,10 +1,10 @@
-let n =  2;
+const n = parseFloat (prompt("Digite um Número: "));
 let res = ""
 
 for(let i= 1 ; i <= 20 ; i++){
     res += + n + " X " + i + " = " + n*i +"\n"
 }
-alert(res)
+alert("Resultado da tabuada do N° " + n + "\n" + res)
 
 
 
