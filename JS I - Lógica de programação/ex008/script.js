fa80abf70b24b9a5935b46d124b5742144f2644a
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-15 09:41:41 -0300

    adicionando elementos no array

---
+++ JS I - Lógica de programação/ex008/script.js
@@ -0,0 +1,8 @@
+let arr = ["café", "almoço", "janta"]
+//adiciona um elemento no final do array
+arr.push ("ceia")
+console.log (arr)
+//adiciona um elemento no começo do array
+arr.unshift("ceia")
+console.log(arr)
+
