b9cef26a3cec1ff1ccdd6e625636e1bebae7890c
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-21 10:24:05 -0300

    Objeto JS

---
+++ JS I - Lógica de programação/ex010-objetos/script.js
@@ -0,0 +1,20 @@
+let cadastro = {}
+
+cadastro.nome = "Muryllo"
+cadastro.idade = 26
+cadastro.sexo = "Masculino"
+cadastro.cor = "parda"
+
+//console.log(cadastro)
+//console.log(cadastro.nome)
+
+cadastro.nome = "joão"
+//console.log(cadastro.nome)
+//console.log(cadastro)
+
+let endereço = ["Rua: Av.José Manoel Viela", "n° 1948", "Bairro: Vila Regina", "Cidade: Santa Rita Do Araguaia - GO"]
+cadastro.endereço = endereço
+//console.log(cadastro.endereço)
+
+console.log(cadastro)
+
