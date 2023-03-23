c0b1ef4f40df3a8c091127c2cbd3d94c36cac896
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-23 14:17:12 -0300

    escopo de funções

---
+++ JS I - Lógica de programação/ex011-Funcoes/escopo/script.js
@@ -0,0 +1,21 @@
+let nome = "Muryllo"
+
+function outroNome(){
+    nome = "Jão"
+}
+
+//console.log(nome)
+//outroNome()
+//console.log(nome)
+
+function criarAnimal() {
+ let animal = "Gato"
+ 
+  }
+  criarAnimal()
+  console.log(animal)
+ 
+
+
+
+
