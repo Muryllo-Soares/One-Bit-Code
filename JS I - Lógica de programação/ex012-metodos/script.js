7dc32b1e6b4ebb008c510f6057cdc403866eced6
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-03-24 17:58:47 -0300

    metodos

---
+++ JS I - Lógica de programação/ex012-metodos/script.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: "Muryllo",
+    idade: "26",
+    dizerOla(){
+        console.log("Olá Mundo!, Meu nome é " + this.nome)
+    }
+}
+
+console.log(pessoa)
+
+alert (pessoa.dizerOla())
--- JS I - Lógica de programação/ex012/script.js
+++
@@ -1,7 +0,0 @@
-let notas = [10, 8, 5]
-function media(){
-    return (notas[0]+notas[1]+notas[2]) / notas.length
-}
-console.log(media())
-
-
