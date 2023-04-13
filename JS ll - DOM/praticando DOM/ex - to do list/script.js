c1a16e755d3902ed46ad86701f85b8ba316bd59e
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-04-13 15:11:39 -0300

    projetinho to de list

---
+++ JS ll - DOM/praticando DOM/ex - to do list/script.js
@@ -0,0 +1,21 @@
+const addBtn = document.getElementById('addBtn')
+const inputText = document.getElementById('text')
+const ulList = document.getElementById('list')
+
+addBtn.addEventListener('click', function(){
+
+    const newLi = document.createElement('li')
+    newLi.className = 'newLi'
+    const removeRowBtn = document.createElement('button')
+    removeRowBtn.type = 'button'
+    removeRowBtn.innerText = ' Remover'
+    removeRowBtn.className = 'removeBtn'
+
+    removeRowBtn.addEventListener('click', function(){
+        ulList.removeChild(newLi)
+    })
+
+    newLi.append(inputText.value, removeRowBtn)
+    ulList.append(newLi)
+    inputText.value = ''
+})
