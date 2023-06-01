0d74a4fd0e2f834a2c973205c7bab6043695ddb6
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-06-01 17:22:28 -0300

    aprendendo react

---
+++ Fundamentos do React/first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt aliquid at quaerat, minima optio corporis sint esse alias repellat laboriosam consequatur sit animi consequuntur ut. Corporis facere corrupti reiciendis atque.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto rem placeat laborum inventore, tenetur in doloremque recusandae excepturi repellendus tempore suscipit! Quidem in recusandae, necessitatibus excepturi laudantium cupiditate amet voluptatum?</p>
+      <ul>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic maiores suscipit harum, obcaecati, consequatur veritatis asperiores atque molestias temporibus quae autem distinctio itaque earum corporis facilis doloribus perspiciatis nobis sapiente?</li>
+        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nihil eum, ipsa quas sunt minima incidunt laudantium quod maxime assumenda quidem, debitis nisi quam quis, qui suscipit sapiente in mollitia.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum alias impedit, placeat repudiandae labore odit nulla unde enim a quibusdam perspiciatis doloremque nihil totam repellendus ipsam quia rerum dolores aliquid.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, aliquid eum, quas facilis voluptatibus corporis ratione atque in enim, perspiciatis obcaecati? Tempora eum officia eveniet, laborum exercitationem fuga voluptatum ea.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facere tenetur vitae numquam sapiente id ducimus odit aut autem qui, architecto, magni distinctio. Iste suscipit omnis vitae, ea nam distinctio.</li>
+      </ul>
+    </div>
+  );
+}
