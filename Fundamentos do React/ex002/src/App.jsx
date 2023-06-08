8d0c8dc4facef7d023369dad055efa6fe8884a0a
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-06-08 17:48:48 -0300

    update

--- Fundamentos do React/ex002/src/App.jsx
+++ Fundamentos do React/ex002/src/App.jsx
@@ -6,11 +6,11 @@ export default function App(){
       <img src="homem-de-ferro.jpg" alt="Poster Homem de Ferro" />
       <div className='text'>
         <h1>Pôster: Homem De Ferro</h1>
-        
+
         <p>Um pôster decorativo épico do Homem de Ferro, com moldura de MDF e tamanho A3. Uma ótima recomendação de um dos mais conhecidos heróis dos quadrinhos. Este clássico pôster trará aventura, nostalgia e a magia do icônico personagem da Marvel para qualquer lugar que você decidi pendurar. Não perca a chance de adicionar esse lindo pôster ao seu acervo. </p>
 
         <button>
-          Comprar agora
+        Comprar agora
         </button>
       </div>
     </div>
