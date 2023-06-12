973cc518281cf9b6405835bcf306f6e9c08e0860
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-06-12 14:28:28 -0300

    ex 003

--- Fundamentos do React/ex003/src/App.jsx
+++ Fundamentos do React/ex003/src/App.jsx
@@ -7,6 +7,12 @@ export default function App(){
     title = "Avatar Profile" 
     avatar = {avatarProfile}
     name = "Muryllo Soares"
+    bio = "Full-Stack javaScript Developer at Acme Inc."
+    phone = "+556699999-9999"
+    email = "muryllo@email.com"
+    GitHub = "GitHub"
+    Linkedin = "LinkedIn"
+    twitter = "Twitter"
     />
   )
 }
--- Fundamentos do React/ex003/src/Components/Profile/Profile.jsx
+++ Fundamentos do React/ex003/src/Components/Profile/Profile.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/prop-types */
 import style from "./Profile.module.css";
 
-export default function Profile({ avatar, title, name }) {
+export default function Profile({ avatar, title, name, bio, phone, email, GitHub, Linkedin, twitter }) {
   return (
       <div className={style.card}>
+
+        <br />
+
         <img  src={avatar} alt={title} />
-        <p>{name}</p>
-        <hr />
-        <p>Full-STACK</p>
+
+        <div className={style.profile}>
+          <h2>{name}</h2>
+          <p>{bio}</p>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
+
+        <br />
+
+        <div className={style.link}>
+        <a href="https://github.com/Muryllo-Soares">{GitHub}</a>
+        <a href="https://www.linkedin.com/in/muryllo-soares-76a981118/">{Linkedin}</a>
+        <a href="https://twitter.com/Muzer41">{twitter}</a>
+        </div>
       </div>
   );
 }
