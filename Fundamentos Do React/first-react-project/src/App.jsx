e8563c71480515927c477da5a079a9aef5164b12
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-09-11 14:46:01 -0300

    Update App.jsx

--- Fundamentos Do React/first-react-project/src/App.jsx
+++ Fundamentos Do React/first-react-project/src/App.jsx
@@ -5,11 +5,11 @@ export default function App(){
         <p>Este é o meu primeiro projeto usando React e vite!</p>
         <p>Próximos passos:</p>
         <ul>
-          <li>Praticar com exercícios</li>
-          <li>Aprender a usar dados dinâmicos</li>
-          <li>Criar novos componentes</li>
-          <li>Estilizar os componentes</li>
-          <li>Reutilizar componentes</li>
+          <li>Praticar com exercícios.</li>
+          <li>Aprender a usar dados dinâmicos.</li>
+          <li>Criar novos componentes.</li>
+          <li>Estilizar os componentes.</li>
+          <li>Reutilizar componentes.</li>
         </ul>
       </div>
   )
