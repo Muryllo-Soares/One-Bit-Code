9edd0d20471dc6c5dfaff2d54a100f573fcf838c
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-09-11 17:31:28 -0300

    ex001

---
+++ Fundamentos Do React/ex001/src/App.jsx
@@ -0,0 +1,44 @@
+
+export default function App() {
+  return (
+    <div>
+      <img src="../public/react.png" alt="React" />
+      <h2>
+        React
+      </h2>
+      <p>
+        A biblioteca para interfaces de usuário web nativas.
+      </p>
+      <button>Aprenda React</button>
+      <button>Referência de API</button>
+      <hr />
+      <h2>
+        Crie interfaces de usuário de componentes
+      </h2>
+      <p>
+        React permite que você construa interfaces de usuário a partir de
+        pedaços individuais chamados componentes.
+      </p>
+      <hr />
+      <h2>
+        Escreva componentes com código e marcação
+      </h2>
+      <p>
+        Componentes React são funções JavaScript. A sintaxe de marcação é
+        chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo
+        React.
+      </p>
+      <hr />
+      <h2>
+        Próximos passos
+      </h2>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Uso de props e cildren</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  );
+}
