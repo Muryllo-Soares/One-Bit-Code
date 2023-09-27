8a2fe654e2d0946cad6f9871e830297d7923d562
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-09-27 13:57:12 -0300

    style ex002

--- Fundamentos Do React/ex002/src/App.jsx
+++ Fundamentos Do React/ex002/src/App.jsx
@@ -1,15 +1,12 @@
-import styles from './App.module.css'
+import Card from "./components/Card";
+
+
 
 export default function App(){
-  return(
-    <div className={styles.container}>
-        <img className= {styles.card} src="../public/card.jpg" alt="imagem do card"/>
-        <div>
-            <h1 className={styles.title}>Pôster: Star Wars (1977)</h1>
-            <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os  tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linha memória ao seu acervo!</p>
-            <button className={styles.button}>Comprar agora</button>
+    return (
+      <div>
+       <Card />
       </div>
-  </div>
-  )
+    )
 
 }
