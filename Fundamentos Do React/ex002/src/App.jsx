f0fe0148a3ac04f8b5912ca405cc86a632af9c3a
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-10-02 21:41:35 -0300

    uso de props

--- Fundamentos Do React/ex002/src/App.jsx
+++ Fundamentos Do React/ex002/src/App.jsx
@@ -1,12 +1,18 @@
 import Card from "./components/Card";
-
+import swPosterImg from "../public/sw-poster.jpg"
+import esbPosterImg from "../public/esb-poster.jpeg"
+import rotjPosterImg from "../public/rotj-poster.jpeg"
 
 
 export default function App(){
     return (
-      <div>
-       <Card />
-      </div>
+      <>
+       <Card title = "Pôster: Star Wars (1977)" poster={swPosterImg} />
+       <Card title = "Pôster: Empire Strikes Back (1980)" poster={esbPosterImg} />
+       <Card title = "Pôster: Return of the Jedi (1983)" poster = {rotjPosterImg} />
+      </>
+      
+      
     )
 
 }
