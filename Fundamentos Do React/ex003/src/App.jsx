f9c0f863b51a410619340e2d948cae29eb8433f4
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-10-04 10:33:24 -0300

    Atividade n° 3
    nesta atividade foi pedido que criasse um card profile utilizando props e alguns estilos usando CSS

--- Fundamentos Do React/ex003/src/App.jsx
+++ Fundamentos Do React/ex003/src/App.jsx
@@ -1,10 +1,19 @@
-import Card from './Components/Card/index'
-import style from "/style.module.css"
+import Profile from "./Components/Profile"
+import avatar from "./assets/profile.jpg"
 
 export default function App(){
   return (
-    <div className={style.app}>
-      <Card/>
+    <div className="app">
+      <Profile
+        avatar = {avatar}
+        name = "Muryllo Soares"
+        bio = "Full-stack javascript developer"
+        phone = "+556699999-9999"
+        email = "muryllo@email.com"
+        githubUrl = "https://github.com/Muryllo-Soares"
+        linkedinUrl = "https://www.linkedin.com/in/muryllo-soares-76a981118/"
+        twitterUrl = "https://twitter.com/home"
+      />
     </div>
   )
 }
