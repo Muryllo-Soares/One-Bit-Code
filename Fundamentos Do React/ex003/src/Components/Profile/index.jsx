3b71c18c1f8b8d1d13283ab8eb15a270ec2e1be4
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-10-19 15:26:47 -0300

    Usando eventos JavaScript no React

--- Fundamentos Do React/ex003/src/Components/Profile/index.jsx
+++ Fundamentos Do React/ex003/src/Components/Profile/index.jsx
@@ -5,11 +5,24 @@ import ProfileSection from "../ProfileSection"
 import Title from "../Title"
 import style from "./style.module.css"
 
+function hundleClick (ev){
+    console.log(ev)
+    alert("Agora você está seguindo")
+}
+
 export default function Profile(props){
     return (
         <div className={style.container}>
             <img className={style.avatar} src={props.avatar} alt={props.name} />
-            <Title className={style.name}>{props.name}</Title>
+            <Title className={style.name}>
+                {props.name}
+                <button
+                className={style.followButton}
+                onClick={hundleClick}
+                >
+                    Follow
+                </button>
+            </Title>
             <ProfileSection>{props.bio}</ProfileSection>
             <ProfileSection>{props.phone}</ProfileSection>
             <ProfileSection>{props.email}</ProfileSection>
