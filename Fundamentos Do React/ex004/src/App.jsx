8409d861535f13a3fbdbadd43a82572a847118be
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-10-25 17:14:58 -0300

    incremento de logica no exercicio de criar senha

--- Fundamentos Do React/ex004/src/App.jsx
+++ Fundamentos Do React/ex004/src/App.jsx
@@ -2,28 +2,40 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-  const [copiar, setCopiar] = useState("Copiar")
-  const [gerar, setGerar] = useState()
+  const [password, setPassword] = useState("")
+  const [ copyText, setCopyText] = useState("Copiar")
 
- function handleClick(){
-  setGerar("_YBw#RF&WHUM")
-  setCopiar("Copiar")
- }
+  function generate (){
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    const length = 12
+    let newPassword = ""
+    for (let i = 0; i < length; i++) {
+        const position = Math.floor(Math.random() * characters.length)
+        newPassword +=  characters[position]
+    }
+    setPassword(newPassword)
+    setCopyText("Copiar")
+  }
+
+  function copyToClipboard () {
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+ 
 
   return (
     <>
       <h1>Gerador de senhas</h1>
       <div className="card">
-        <button onClick={() => handleClick()}>
+        <button onClick={generate}>
           Gerar! 
         </button>
 
-        <button onClick={()=> setCopiar("Copiado!")}>
-        {copiar}
+        <button onClick={copyToClipboard}>
+        {copyText}
         </button>
 
-        <p>{gerar}</p>
+        <p>{password}</p>
       </div>
     </>
   )
